a38ca7bd0a3f9fa2d5f189fb32be1421d2bb00ea
Viktor Pavlyshyn <lucean7@gmail.com>
2021-02-26 19:38:18 +0200

    fix whit stream and try

--- src/main/java/com/epam/businessobject/BOProvider.java
+++ src/main/java/com/epam/businessobject/BOProvider.java
@@ -1,20 +1,19 @@
 package com.epam.businessobject;
 
+import java.util.Objects;
+
 public class BOProvider {
-//    TODO:
-//    Try to rewrite using Optional
-//    If you need verify if null, there is a native Objects.isNull() method
-//
+
 //    TODO:
 //    Rewrite usign Factory which will create by class name
     private GoogleSearchBO googleSearchBO;
     private ResultSearchGoogleBO resultSearchGoogleBO;
 
     public GoogleSearchBO getGoogleSearchBO() {
-        return (googleSearchBO == null) ? googleSearchBO = new GoogleSearchBO() : googleSearchBO;
+        return Objects.isNull(googleSearchBO) ? googleSearchBO = new GoogleSearchBO() : googleSearchBO;
     }
 
     public ResultSearchGoogleBO getResultSearchGoogleBO() {
-        return (resultSearchGoogleBO == null) ? resultSearchGoogleBO = new ResultSearchGoogleBO() : resultSearchGoogleBO;
+        return Objects.isNull(resultSearchGoogleBO) ? resultSearchGoogleBO = new ResultSearchGoogleBO() : resultSearchGoogleBO;
     }
 }
