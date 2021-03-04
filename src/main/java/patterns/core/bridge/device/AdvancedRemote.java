26898d58fb382db2e8d79f4d6e53e3f5d660a087
Viktor Pavlyshyn <lucean7@gmail.com>
2021-03-04 21:54:10 +0200

    add bridge pattern

---
+++ src/main/java/patterns/core/bridge/device/AdvancedRemote.java
@@ -0,0 +1,16 @@
+package patterns.core.bridge.device;
+
+import patterns.core.bridge.BasicRemote;
+import patterns.core.bridge.Device;
+
+public class AdvancedRemote extends BasicRemote {
+
+    public AdvancedRemote(Device device) {
+        super.device = device;
+    }
+
+    public void mute() {
+        System.out.println("Remote: mute");
+        device.setVolume(0);
+    }
+}
