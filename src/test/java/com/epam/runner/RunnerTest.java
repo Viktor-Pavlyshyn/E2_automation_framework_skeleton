c505614885e7a1367142d283f2a37c70b5f477de
Viktor Pavlyshyn <lucean7@gmail.com>
2021-03-25 22:00:24 +0200

    add sauce lads and selenide

--- src/test/java/com/epam/runner/RunnerTest.java
+++ src/test/java/com/epam/runner/RunnerTest.java
@@ -2,10 +2,14 @@ package com.epam.runner;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/resources/features"},
-        glue = {"com/epam/definitions"})
+@CucumberOptions(plugin = {"pretty",
+        "message:target/cucumber-report",
+        "io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm"},
+        features = {"src/test/resources/features"},
+        glue = {"com/epam/definitions"}, tags = "@selenide")
 public class RunnerTest {
 }
