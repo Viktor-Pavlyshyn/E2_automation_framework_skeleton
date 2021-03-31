ae8d5dd1eccae31eef3f54b5880c5085f13ebc44
Viktor Pavlyshyn <lucean7@gmail.com>
2021-03-31 21:37:46 +0300

    add com.codeborne.selenide.impl.Waiter;

--- src/main/java/selenide/page/object/RefAboutWikipediaPage.java
+++ src/main/java/selenide/page/object/RefAboutWikipediaPage.java
@@ -1,6 +1,7 @@
 package selenide.page.object;
 
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.impl.Waiter;
 import io.qameta.allure.Step;
 import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
@@ -11,10 +12,20 @@ import static com.codeborne.selenide.Selenide.$x;
 @Getter
 public class RefAboutWikipediaPage {
     private final SelenideElement wikipediaAbout = $x("//h1[@class=\"firstHeading\"]");
+    private final Waiter waiter = new Waiter();
 
     @Step("Get text from Wikipedia About")
-    public String getTextWikipediaAbout(){
+    public String getTextWikipediaAbout() {
         log.info("Get text from Wikipedia About");
+        waiter.wait(wikipediaAbout, this::isDisplayed, 60, 2);
         return wikipediaAbout.getText();
+    }
+
+    private boolean isDisplayed(SelenideElement element) {
+        try {
+            return element.isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
